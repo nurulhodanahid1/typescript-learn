86dd02b224c62a169a2af336c141790e93e6f360
Nurul Hoda Nahid <nurulhodanahid1@gmail.com>
2022-04-24 23:45:25 -0600

    added typescrit type

---
+++ src/script.ts
@@ -0,0 +1,39 @@
+///////////////////// Type ///////////////////
+
+//Variable
+
+// let names = "nahid";
+// names="sarkar"
+// // names= 23;
+// console.log(names)
+
+//Function
+
+// function multiply(a : number, b : number){
+//     return a*b;
+// };
+// let output = multiply(3,3)
+// console.log(output)
+
+//Array
+
+// let fruits = [];
+// fruits.push(23);
+// fruits.push(("nahid"));
+// console.log(fruits);
+
+// let mixed = ["nahid", 22, true, {}]
+// mixed.push(23);
+// mixed.push({color: "green",type: "sobuz"})
+// console.log(mixed)
+
+//Object
+
+// let person = {
+//     name : "John",
+//     age : 23,
+//     isMale : true
+// }
+// person.age = 24;
+// person.isMale = false;
+// console.log(person);
